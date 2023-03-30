05a5c0dafa742743f090495c5c6db7d1d159cef0
Jhunel Peñaflorida <jhunel.penaflorida@novare.com.hk>
2023-03-30 16:44:48 +0800

    Add an animation in the Main Activity

--- app/src/main/java/ph/stacktrek/novare/ecommercenovare/penaflorida_dellota/testsnakeladders/MainActivity.kt
+++ app/src/main/java/ph/stacktrek/novare/ecommercenovare/penaflorida_dellota/testsnakeladders/MainActivity.kt
@@ -3,6 +3,8 @@ package ph.stacktrek.novare.ecommercenovare.penaflorida_dellota.testsnakeladders
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val floatingImage = findViewById<ImageView>(R.id.floating_image)
+        val floatAnimation = AnimationUtils.loadAnimation(this, R.anim.float_animation)
+        floatingImage.startAnimation(floatAnimation)
     }
 
     fun gotos(view: View?) {
