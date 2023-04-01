e481945d9cbb9f1e99ddef0d78dc28ef67ba2d07
Joash Dellota <joash.dellota@novare.com.hk>
2023-04-02 02:51:03 +0800

    +player model +2 player mode +winner name

---
+++ app/src/main/java/ph/stacktrek/novare/ecommercenovare/penaflorida_dellota/testsnakeladders/model/Player.kt
@@ -0,0 +1,8 @@
+package ph.stacktrek.novare.ecommercenovare.penaflorida_dellota.testsnakeladders.model
+
+import android.widget.ImageView
+
+class Player (val name:String){
+    var position:Int=0
+    var pon: ImageView? = null
+}
